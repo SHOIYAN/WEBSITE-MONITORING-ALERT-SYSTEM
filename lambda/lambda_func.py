cd4731ad1b525b3439b4995b40cf74803e0d8dc2
SHOIYAN <shoiyan9@gmail.com>
2025-03-10 11:49:05 +0300

    testing workflow

--- lambda/lambda_func.py
+++ lambda/lambda_func.py
@@ -13,17 +13,8 @@ DYNAMODB_TABLE = os.getenv('DYNAMODB_TABLE','TanaMonitoring')
 SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
 
 def check_website():
-    """Checks website status returns True of False"""
-    url = 'https://www.tanatech.io/'
+    return False  # Force website check to fail
 
-    try:
-        response = requests.get(url, timeout=5)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-    except requests.RequestException:
-        return False
 
 def log_status(status):
     """Logs website status to Dynamodb with a timestamp"""
